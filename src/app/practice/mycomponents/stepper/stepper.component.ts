b06c5e2a94f3553bce6fc3cb341c45fa179bb44b
Hemapreetha5 <hemapreetha96@gmail.com>
2024-11-04 20:08:53 +0530

    stepper implemented

--- src/app/practice/mycomponents/stepper/stepper.component.ts
+++ src/app/practice/mycomponents/stepper/stepper.component.ts
@@ -1,19 +1,49 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-
+// import { Component, inject } from '@angular/core';
+// import { FormBuilder, Validators,FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.css']
 })
 export class StepperComponent {
-  private _formBuilder = inject(FormBuilder);
+  // private _formBuilder = inject(FormBuilder);
 
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  isLinear = false;
+  // firstFormGroup = this._formBuilder.group({
+  //   firstCtrl: ['', Validators.required],
+  // });
+  // secondFormGroup = this._formBuilder.group({
+  //   secondCtrl: ['', Validators.required],
+  // });
+  // isLinear = false;
+
+
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+  isLinear = true;
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    // Step 1 form group
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+
+    // Step 2 form group
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+  // Method to print form values to the console
+  saveForm() {
+    if (this.firstFormGroup.valid && this.secondFormGroup.valid) {
+      console.log('First Step Value:', this.firstFormGroup.value);
+      console.log('Second Step Value:', this.secondFormGroup.value);
+    } else {
+      console.log('Please complete the form!');
+    }
+  }
 }
